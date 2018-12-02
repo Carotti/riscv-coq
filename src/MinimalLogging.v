d167f7a718379fbd2416fccc24ecacd636fd3554
Thomas Carotti <thomas@carotti.co.uk>
2018-12-02 17:58:46 -0500

    Revert "Add Axiomatic logging riscv"
    This reverts commit ff1414eb1bc80e79b45f3e8eb80d3d04b6070ec2.

--- src/MinimalLogging.v
+++ src/MinimalLogging.v
@@ -10,7 +10,6 @@ Require Import riscv.util.PowerFunc.
 Require Import riscv.Utility.
 Require Import Coq.Lists.List. Import ListNotations.
 Require Import riscv.Minimal.
-Require Import riscv.AxiomaticRiscv.
 
 Section Riscv.
 
@@ -35,17 +34,6 @@ Section Riscv.
 
   Definition putProgram{Log : Type}(prog: list (word 32))(addr: t)(ma: @RiscvMachineLog Log): @RiscvMachineLog Log :=
     with_machine (putProgram prog addr ma) ma.
-
-  Definition with_registers_log{Log : Type} r (ma: @RiscvMachineLog Log) :=
-    (@with_machine Log) (with_registers r ma) ma.
-  Definition with_pc_log{Log : Type} p (ma: @RiscvMachineLog Log) :=
-    (@with_machine Log) (with_pc p ma) ma.
-  Definition with_nextPC_log{Log : Type} npc (ma: @RiscvMachineLog Log) :=
-    (@with_machine Log) (with_nextPC npc ma) ma.
-  Definition with_exceptionHandlerAddr_log{Log : Type} eh (ma: @RiscvMachineLog Log) :=
-    (@with_machine Log) (with_exceptionHandlerAddr eh ma) ma.
-  Definition with_machineMem_log{Log : Type} m (ma: @RiscvMachineLog Log) :=
-    (@with_machine Log) (with_machineMem m ma) ma.
 
   Definition liftL0{B Log: Type}(f: OState RiscvMachine B):  OState RiscvMachineLog B :=
     fun (s : @RiscvMachineLog Log) => let (ob, ma) := f s in (ob, with_machine ma s).
@@ -145,93 +133,6 @@ Section Riscv.
       getCSRField_MTVecBase := liftL0 getCSRField_MTVecBase;
       endCycle A := Return None;
   |}.
-  
-   Class AxiomaticRiscvLog :=  mkAxiomaticRiscvLog {
-      
-      Bind_getRegister0: forall {A: Type} (f: t -> OState RiscvMachineLog A),
-        Bind (getRegister Register0) f = f (ZToReg 0);
-      
-      Bind_getRegister: forall {A: Type} x (f: t -> OState RiscvMachineLog A)
-                          (initialL: RiscvMachineLog),
-          valid_register x ->
-          (Bind (getRegister x) f) initialL =
-          (f (getReg initialL.(core).(registers) x)) initialL;
-
-      Bind_setRegister: forall {A: Type} x (v: t)
-                          (f: unit -> OState RiscvMachine A) (initialL: RiscvMachine),
-          valid_register x ->
-          (Bind (setRegister x v) f) initialL =
-          (f tt) (with_registers (setReg initialL.(core).(registers) x v) initialL);
-
-      Bind_setRegister0: forall {A: Type} (v: t)
-                           (f: unit -> OState RiscvMachine A) (initialL: RiscvMachine),
-          (Bind (setRegister Register0 v) f) initialL =
-          (f tt) initialL;
-
-      Bind_loadByte: forall {A: Type} (addr: t) (f: word 8 -> OState RiscvMachine A)
-                       (initialL: RiscvMachine),
-          (Bind (loadByte addr) f) initialL =
-          (f (Memory.loadByte initialL.(machineMem) addr)) initialL;
-
-      Bind_loadHalf: forall {A: Type} (addr: t) (f: word 16 -> OState RiscvMachine A)
-                       (initialL: RiscvMachine),
-          (Bind (loadHalf addr) f) initialL =
-          (f (Memory.loadHalf initialL.(machineMem) addr)) initialL;
-
-      Bind_loadWord: forall {A: Type} (addr: t) (f: word 32 -> OState RiscvMachine A)
-                       (initialL: RiscvMachine),
-          (Bind (loadWord addr) f) initialL =
-          (f (Memory.loadWord initialL.(machineMem) addr)) initialL;
-      
-      Bind_loadDouble: forall {A: Type} (addr: t) (f: word 64 -> OState RiscvMachine A)
-                       (initialL: RiscvMachine),
-          (Bind (loadDouble addr) f) initialL =
-          (f (Memory.loadDouble initialL.(machineMem) addr)) initialL;
-
-      Bind_storeByte: forall {A: Type} (addr: t) (v: word 8)
-                        (f: unit -> OState RiscvMachine A) (initialL: RiscvMachine),
-          (Bind (storeByte addr v) f) initialL =
-          (f tt) (with_machineMem (Memory.storeByte initialL.(machineMem) addr v)
-                                            initialL);
-
-      Bind_storeHalf: forall {A: Type} (addr: t) (v: word 16)
-                        (f: unit -> OState RiscvMachine A) (initialL: RiscvMachine),
-          (Bind (storeHalf addr v) f) initialL =
-          (f tt) (with_machineMem (Memory.storeHalf initialL.(machineMem) addr v)
-                                            initialL);
-
-      Bind_storeWord: forall {A: Type} (addr: t) (v: word 32)
-                        (f: unit -> OState RiscvMachine A) (initialL: RiscvMachine),
-          (Bind (storeWord addr v) f) initialL =
-          (f tt) (with_machineMem (Memory.storeWord initialL.(machineMem) addr v)
-                                            initialL);
-
-      Bind_storeDouble: forall {A: Type} (addr: t) (v: word 64)
-                        (f: unit -> OState RiscvMachine A) (initialL: RiscvMachine),
-          (Bind (storeDouble addr v) f) initialL =
-          (f tt) (with_machineMem (Memory.storeDouble initialL.(machineMem) addr v)
-                                            initialL);
-
-      Bind_getPC: forall {A: Type} (f: t -> OState RiscvMachine A) (initialL: RiscvMachine),
-          (Bind getPC f) initialL =
-          (f initialL.(core).(pc)) initialL;
-
-      Bind_setPC: forall {A: Type} (v: t)
-                    (f: unit -> OState RiscvMachine A) (initialL: RiscvMachine),
-          (Bind (setPC v) f) initialL =
-          (f tt) (with_nextPC v initialL);
-      
-      Bind_step: forall {A: Type} (f: unit -> OState RiscvMachine A) m,
-          (Bind step f) m =
-          (f tt) (with_nextPC (add m.(core).(nextPC) (ZToReg 4)) (with_pc m.(core).(nextPC) m));
-
-      execState_step: forall m,
-          step m = (Some tt, with_nextPC_log (add m.(core).(nextPC) (ZToReg 4)) (with_pc_log m.(core).(nextPC) m));
-      
-      execState_Return: forall {S A} (s: S) (a: A),
-          (Return a) s = (Some a, s);
-
-  }.
 
 End Riscv.
 
